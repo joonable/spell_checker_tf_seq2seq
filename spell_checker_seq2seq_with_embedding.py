d02fea3a07265f90023baefcc047d244ee33ff2f
Joon <joonable2@gmail.com>
2018-05-14 14:37:41 +0900

    spell checker based on 'spell_checker_seq2seq.py' with embedding

--- spell_checker_seq2seq_with_embedding.py
+++ spell_checker_seq2seq_with_embedding.py
@@ -12,45 +12,39 @@ class SpellChecker():
         self.n_hidden = config['n_hidden']
         self.total_epoch = config['total_epoch']
         self.batch_size = config['batch_size']
+        self.embedding_size = config['embedding_size']
         self.char_arr = list("SEPabcdefghijklmnopqrstuvwxyz ")
-        self.n_class = self.n_input = self.dic_len = len(self.char_arr)
         self.num_dic = {n:i for i, n in enumerate(self.char_arr)}
+        self.n_class = self.n_input = self.dic_len = len(self.char_arr)
         self.n_eval = config['n_eval']
 
-        self.training_mode = tf.placeholder(tf.bool, [None, None], name = "training_mode")
+        self.training_mode = True
         self.output_keep_prob = tf.placeholder(tf.float32)
 
         # Checkpoint files will be saved in this directory during training
-        timestamp = str(int(time.time()))
-        self.checkpoint_dir = './checkpoints_' + timestamp + '/'
+        self.timestamp = str(int(time.time()))
+        self.checkpoint_dir = './checkpoints_' + self.timestamp + '/'
         if os.path.exists(self.checkpoint_dir):
             shutil.rmtree(self.checkpoint_dir)
         os.makedirs(self.checkpoint_dir)
         self.checkpoint_prefix = os.path.join(self.checkpoint_dir, 'model')
 
-        self.encoder_inputs = tf.placeholder(dtype = tf.float32, shape = [None, None, self.n_input],
+        self.encoder_inputs = tf.placeholder(dtype = tf.int32, shape = [None, None],
                                              name = "encoder_inputs")
-        self.decoder_inputs = tf.placeholder(dtype = tf.float32, shape = [None, None, self.n_input],
+        self.decoder_inputs = tf.placeholder(dtype = tf.int32, shape = [None, None],
                                              name = "decoder_inputs")
-        self.decoder_outputs = tf.placeholder(tf.int64, [None, None, self.n_class], name = "decoder_outputs")
+        self.decoder_outputs = tf.placeholder(tf.int64, [None, None], name = "decoder_outputs")
         self.target_weights = tf.placeholder(tf.float32, [None, None], name = "target_weights")
 
         self.encoder_length = tf.placeholder(tf.int32, [None], name = "encoder_length")
         self.decoder_length = tf.placeholder(tf.int32, [None], name = "decoder_length")
 
-        # Embedding
-        # Look up embedding:
-        #   encoder_inputs: [max_time, batch_size]
-        #   encoder_emb_inp: [max_time, batch_size, embedding_size]
-        self.embedding_size = 4
-
-        self.embedding = tf.get_variable("embedding", [self.dic_len, self.embedding_size])
-        self.encoder_emb_inp = tf.nn.embedding_lookup(self.embedding, self.encoder_inputs)
-        self.decoder_emb_inp = tf.nn.embedding_lookup(self.embedding, self.decoder_inputs)
-        self.decoder_emb_outp = tf.nn.embedding_lookup(self.embedding, self.decoder_outputs)
-
-        # [batch_size, time_steps, input_size]
         self.global_step = tf.Variable(0, name = 'global_step', trainable = False)
+
+        with tf.variable_scope('embedding'):
+            self.embedding = tf.get_variable("embedding_layer", [self.dic_len, self.embedding_size], trainable = True)
+            self.encoder_emb_inp = tf.nn.embedding_lookup(self.embedding, self.encoder_inputs)
+            self.decoder_emb_inp = tf.nn.embedding_lookup(self.embedding, self.decoder_inputs)
 
         with tf.variable_scope('encode'):
             self.enc_cell = tf.nn.rnn_cell.BasicLSTMCell(self.n_hidden)
@@ -63,12 +57,7 @@ class SpellChecker():
             self.dec_cell = tf.nn.rnn_cell.DropoutWrapper(self.dec_cell, output_keep_prob = self.output_keep_prob)
             self.projection_layer = tf.layers.Dense(self.dic_len, use_bias = True)
 
-            if self.training_mode == True:
-                self.helper = tf.contrib.seq2seq.TrainingHelper(self.decoder_emb_inp, self.decoder_length)
-            else:
-                self.helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(self.embedding,
-                                                                       start_tokens = tf.tile([0], [self.batch_size]),
-                                                                       end_token = 1)
+            self.helper = tf.contrib.seq2seq.TrainingHelper(self.decoder_emb_inp, self.decoder_length)
             self.decoder = tf.contrib.seq2seq.BasicDecoder(self.dec_cell, self.helper, self.enc_states,
                                                            output_layer = self.projection_layer)
 
@@ -78,12 +67,13 @@ class SpellChecker():
             self.logits = self.outputs.rnn_output
             self.prediction = tf.argmax(self.logits, axis = 2)
 
-        with tf.variable_scope('cost'):
-            self.crossent = tf.nn.softmax_cross_entropy_with_logits(logits = self.logits, labels = self.decoder_emb_outp)
+        with tf.variable_scope('Cost'):
+            self.crossent = tf.nn.sparse_softmax_cross_entropy_with_logits(logits = self.logits,
+                                                                           labels = self.decoder_outputs)
             self.cost = (tf.reduce_mean(self.crossent * self.target_weights))
             tf.summary.scalar('cost', self.cost)
 
-        with tf.variable_scope('accuracy'):
+        with tf.variable_scope('Accuracy'):
             correct_predictions = tf.equal(self.prediction, self.decoder_outputs)
             self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"))
             tf.summary.scalar('accuracy', self.accuracy)
@@ -96,10 +86,8 @@ class SpellChecker():
             self.train_op = self.opimiser.apply_gradients(zip(self.clipped_gradients, self.params),
                                                           global_step = self.global_step)
 
-        self.df_train = pd.read_csv('./df_train.csv', index_col = False)
-        self.df_test = pd.read_csv('./df_test.csv', index_col = False)
-        self.df_train = self.df_train[['x', 'y']]
-        self.df_test = self.df_test[['x', 'y']]
+        self.df_train = pd.read_csv('./df_train.csv', index_col = False)[['x', 'y']]
+        self.df_test = pd.read_csv('./df_test.csv', index_col = False)[['x', 'y']]
 
         self.graph = tf.Graph()
         self.saver = tf.train.Saver()
@@ -118,7 +106,7 @@ class SpellChecker():
                 end_index = min((batch_num + 1) * batch_size, data_size)
                 yield data[start_index:end_index]
 
-    def make_batch(self, df):
+    def make_batch_emb(self, df):
         enc_input_batch = []
         dec_input_batch = []
         dec_output_batch = []
@@ -150,15 +138,13 @@ class SpellChecker():
             output = output + [2] * (dec_max_len - len(output))
             target = target + [2] * (dec_max_len - len(target))
 
-            enc_input_batch.append(np.eye(self.dic_len)[input])
-            dec_input_batch.append(np.eye(self.dic_len)[output])
+            enc_input_batch.append(input)
+            dec_input_batch.append(output)
             dec_output_batch.append(target)
 
-        # target_weights_batch = tf.squeeze(target_weights_batch)
         return enc_input_batch, dec_input_batch, dec_output_batch, target_weights_batch, enc_len_batch, dec_len_batch
 
     def train(self):
-        self.training_mode = True
         x_train = self.df_train.x.tolist()
         y_train = self.df_train.y.tolist()
         train_batches = self.batch_iter(data = list(zip(x_train, y_train)), batch_size = self.batch_size,
@@ -168,7 +154,8 @@ class SpellChecker():
         for train_batch in train_batches:
             current_step = tf.train.global_step(self.sess, self.global_step)
             train_enc_input_batch, train_dec_input_batch, train_dec_output_batch, train_target_weights_batch, train_enc_len_batch, train_dec_len_batch \
-                = self.make_batch(pd.DataFrame(train_batch, columns = ['x', 'y']))
+                = self.make_batch_emb(pd.DataFrame(train_batch, columns = ['x', 'y']))
+
             feed_dict = {
                 self.encoder_inputs:train_enc_input_batch,
                 self.decoder_inputs:train_dec_input_batch,
@@ -176,8 +163,7 @@ class SpellChecker():
                 self.target_weights:train_target_weights_batch,
                 self.encoder_length:train_enc_len_batch,
                 self.decoder_length:train_dec_len_batch,
-                self.output_keep_prob:0.75,
-                self.training_mode: True
+                self.output_keep_prob:0.75
             }
             self.merged_summaries = tf.summary.merge_all()
             _, loss, accuracy, summary = self.sess.run([self.train_op, self.cost, self.accuracy, self.merged_summaries],
@@ -189,7 +175,7 @@ class SpellChecker():
 
             if current_step % self.n_eval == 0:
                 val_enc_input_batch, val_dec_input_batch, val_dec_output_batch, val_target_weights_batch, val_enc_len_batch, val_dec_len_batch \
-                    = self.make_batch(pd.DataFrame(train_batch, columns = ['x', 'y']))
+                    = self.make_batch_emb(pd.DataFrame(train_batch, columns = ['x', 'y']))
 
                 val_feed_dict = {
                     self.encoder_inputs:val_enc_input_batch,
@@ -198,8 +184,7 @@ class SpellChecker():
                     self.target_weights:val_target_weights_batch,
                     self.encoder_length:val_enc_len_batch,
                     self.decoder_length:val_dec_len_batch,
-                    self.output_keep_prob:1,
-                    self.training_mode:False
+                    self.output_keep_prob:1
                 }
 
                 val_loss, val_accuracy = self.sess.run([self.cost, self.accuracy], feed_dict = val_feed_dict)
@@ -215,11 +200,11 @@ class SpellChecker():
                     print('Best accuracy {} and {} at step {}'.format(train_best_accuracy, val_best_accuracy,
                                                                       self.best_at_step))
 
-    def test(self):
-        self.saver.restore(self.sess, save_path = self.checkpoint_prefix + '-' + str(self.best_at_step))
+    def test(self, df, file_name):
+        self.saver.restore(self.sess, self.checkpoint_prefix + '-' + str(self.best_at_step))
 
-        enc_input_batch, dec_input_batch, dec_output_batch, target_weights_batch, enc_len_batch, dec_len_batch = self.make_batch(
-            self.df_train)
+        enc_input_batch, dec_input_batch, dec_output_batch, target_weights_batch, enc_len_batch, dec_len_batch \
+            = self.make_batch_emb(df)
 
         feed_dict = {
             self.encoder_inputs:enc_input_batch,
@@ -228,22 +213,40 @@ class SpellChecker():
             self.target_weights:target_weights_batch,
             self.encoder_length:enc_len_batch,
             self.decoder_length:dec_len_batch,
-            self.output_keep_prob:1,
-            self.training_mode:False
+            self.output_keep_prob:1
         }
 
         results, loss, accuracy = self.sess.run([self.prediction, self.cost, self.accuracy], feed_dict = feed_dict)
         print('cost = ', '{:.6f}'.format(loss), ', accuracy = ', '{:.6f}'.format(accuracy))
 
-        decoded = []
+        decoded_number = []
         for result in results:
-            decoded.append([self.char_arr[i] for i in result])
+            decoded_number.append([self.char_arr[i] for i in result])
 
-        self.translated = []
-        for result in decoded:
+        decoded_character = []
+        for result in decoded_number:
             try:
                 end = result.index('E')
-                self.translated.append([''.join(result[:end])])
+                decoded_character.append([''.join(result[:end])])
             except:
-                self.translated.append([''.join(result)])
-        return self.translated
+                decoded_character.append([''.join(result)])
+
+        pd.DataFrame(decoded_character).to_csv('./' + file_name + '_result_' + self.timestamp + '.csv', index = False)
+
+
+def main():
+    config = {}
+    config['lr'] = 0.003
+    config['n_hidden'] = 512
+    config['total_epoch'] = 20
+    config['batch_size'] = 256
+    config['n_eval'] = 20
+    config['embedding_size'] = 4
+    spell_checker = SpellChecker(config)
+
+    spell_checker.train()
+    spell_checker.test(spell_checker.df_train, 'train')
+    spell_checker.test(spell_checker.df_test, 'test')
+
+if __name__ == '__main__':
+    main()
